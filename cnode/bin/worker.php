5acb8a1857a5d5205c4abcdb7432d900f2e20b85
konserv <a@konservs.com>
2017-07-10 13:28:24 +0300

    Examples for RabbitMQ

--- cnode/bin/worker.php
+++ cnode/bin/worker.php
@@ -4,23 +4,25 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '_init.php';
 use Brilliant\Log\BLog;
 use Brilliant\Log\BLoggerConsole;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
 
 //
 BLog::RegisterLogger(new BLoggerConsole());
-BLog::addtolog('Starting');
+BLog::addtolog('Waiting for messages...');
 
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-
 $channel->queue_declare('hello', false, false, false, false);
 
-$msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
-echo " [x] Sent 'Hello World!'\n";
+$callback = function($msg) {
+  echo " [x] Received ", $msg->body, "\n";
+};
 
-$channel->close();
-$connection->close();
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
